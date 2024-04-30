0f2fd8d3a617f7ea90fe6490f75e53db6d3b8ea0
arirzwn <aririzwan0@gmail.com>
2024-05-01 00:30:32 +0700

    Memperbaiki breakpoints pada mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSideBar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+
+}
+
+function closeSideBar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+
+}
